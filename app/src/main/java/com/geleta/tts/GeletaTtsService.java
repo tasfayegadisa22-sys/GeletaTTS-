beff128765711ddc281c2851a1b9e06b6d0c3d15
tasfayegadisa22-sys <242479682+tasfayegadisa22-sys@users.noreply.github.com>
2026-04-27 12:48:59 +0300

    Update GeletaTtsService.java

--- app/src/main/java/com/geleta/tts/GeletaTtsService.java
+++ app/src/main/java/com/geleta/tts/GeletaTtsService.java
@@ -1,38 +1,68 @@
 package com.geleta.tts;
 
-import android.speech.tts.TextToSpeechService;
-import android.speech.tts.SynthesisRequest;
 import android.speech.tts.SynthesisCallback;
+import android.speech.tts.SynthesisRequest;
 import android.speech.tts.TextToSpeech;
-import android.media.AudioFormat;
+import android.speech.tts.TextToSpeechService;
+import android.util.Log;
+import java.io.File;
 
 public class GeletaTtsService extends TextToSpeechService {
 
+    private static final String TAG = "GeletaTTS";
+    private File dataDir;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // አፑ የ eSpeak NG ዳታዎችን ከጫንክበት ፎልደር ይፈልጋል
+        dataDir = new File(getExternalFilesDir(null), "geleta-tts-data");
+        Log.d(TAG, "Geleta TTS Service በተሳካ ሁኔታ ተጀምሯል!");
+    }
+
     @Override
     protected int onIsLanguageAvailable(String lang, String country, String variant) {
-        return TextToSpeech.LANG_AVAILABLE;
+        // ስልኩ አማርኛ (am) ከጠየቀ "አለኝ" ብሎ ይመልሳል
+        if ("am".equals(lang)) {
+            return TextToSpeech.LANG_COUNTRY_AVAILABLE;
+        }
+        return TextToSpeech.LANG_NOT_SUPPORTED;
     }
 
     @Override
     protected String[] onGetLanguage() {
-        // ለአማርኛ ቋንቋ
-        return new String[]{"amh", "ETH", ""};
+        // የሞተሩ ነባሪ ቋንቋ አማርኛ (am) መሆኑን ለስልኩ ይነግራል
+        return new String[]{"am", "ET", ""};
     }
 
     @Override
     protected int onLoadLanguage(String lang, String country, String variant) {
-        return TextToSpeech.LANG_AVAILABLE;
+        return onIsLanguageAvailable(lang, country, variant);
     }
 
     @Override
     protected void onStop() {
-        // ድምፅ ማቆሚያ ትዕዛዝ
+        Log.d(TAG, "የማንበብ ሂደቱ ቆሟል");
     }
 
     @Override
     protected void onSynthesizeText(SynthesisRequest request, SynthesisCallback callback) {
-        // ይህ ለጊዜው አፑ ያለምንም ስህተት እንዲገነባ (Compile እንዲያደርግ) የተዘጋጀ መሰረታዊ ኮድ ነው
-        callback.start(16000, AudioFormat.ENCODING_PCM_16BIT, 1);
+        // ይህ ክፍል ስልኩ የላከውን ጽሁፍ የሚቀበልበት ነው
+        String text = request.getCharSequenceText().toString();
+        int speechRate = request.getSpeechRate();
+        int pitch = request.getPitch();
+
+        Log.d(TAG, "የተላከው ጽሁፍ: " + text);
+
+        // ለስልኩ "ማንበብ ጀምሬያለሁ" ብሎ ያሳውቃል
+        callback.start(16000, android.media.AudioFormat.ENCODING_PCM_16BIT, 1);
+
+        /* ማስታወሻ፦ እዚህ ቦታ ላይ ነው ትክክለኛው የ eSpeak C++ ኮድ (JNI) ገብቶ 
+         ጽሁፉን ወደ ድምጽ ሞገድ የሚቀይረው። አሁን የስልኩን ሲስተም (Android TTS API) 
+         ከአፑ ጋር በሚገባ አገናኝተናል! 
+        */
+
+        // ለስልኩ "አንብቤ ጨረስኩ" ብሎ ያሳውቃል
         callback.done();
     }
-}    
+}
